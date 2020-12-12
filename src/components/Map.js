9e350e94abe793bb62a93105e2470abe046871ce
David J Sealey <david.sealey@soak.co.uk>
2020-12-12 13:46:30 +0000

    Feature: Marker info box functionality

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
 const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
     const markers = eventData.map(ev => {
-        if(ev.categories[0].id === 8) {
-            return <LocationMarker key={ev.id} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+        if (ev.categories[0].id === 8) {
+            return <LocationMarker 
+                key={ev.id} 
+                lat={ev.geometries[0].coordinates[1]} 
+                lng={ev.geometries[0].coordinates[0]} 
+                onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
         }
         return null
     })
@@ -13,11 +21,12 @@ const Map = ({ eventData, center, zoom }) => {
         <div className="map">
             <GoogleMapReact
                 bootstrapURLKeys={{ key: 'AIzaSyDhqkjJ7zIrHFarK694dCrSW4aq5VhqzTA' }}
-                defaultCenter={ center }
-                defaultZoom={ zoom }
+                defaultCenter={center}
+                defaultZoom={zoom}
             >
                 {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
